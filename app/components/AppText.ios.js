3843fbf330cd91afa3192c9e0219640e2817fe97
jsacap <sanchoalegre@gmail.com>
2024-04-03 15:29:39 +1100

    platform specific AppText

--- app/components/AppText.ios.js
+++ app/components/AppText.ios.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, StyleSheet, Platform } from 'react-native'
+import { Text, StyleSheet } from 'react-native'
 
 function AppText({children}) {
     return (
@@ -11,9 +11,10 @@ function AppText({children}) {
 
 const styles = StyleSheet.create({
     text: {
-        color: '#000',
-        
-    }
+        fontSize: 18,
+        fontFamily: 'Avenir'
+        }
+    
 })
 
 export default AppText;
