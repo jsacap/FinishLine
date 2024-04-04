858dfb2278872028aa9a633215765559352ada51
jsacap <sanchoalegre@gmail.com>
2024-04-04 13:36:08 +1100

    AppText style fix

--- app/components/AppText/AppText.js
+++ app/components/AppText/AppText.js
@@ -3,7 +3,7 @@ import { Text, StyleSheet, Platform } from 'react-native'
 
 function AppText({children, style}) {
     return (
-        <Text style={[styles.text, style]}>
+        <Text style={[styles.text, {...style}]}>
             {children}
         </Text>
     );
--- app/components/IconButton.js
+++ app/components/IconButton.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import { FontAwesome5 } from '@expo/vector-icons';
+
+import { StyleSheet } from 'react-native';
+
 import colors from '../config/colors';
 
-function IconButton({ iconName, size, color }) {
+function IconButton({ iconName, size, color, style }) {
     return (
-        <FontAwesome5 name={iconName} size={size || 24} color={color || colors.black} />
+        <FontAwesome5 
+        name={iconName} 
+        size={size || 24} 
+        color={color || colors.white} 
+        style={[styles.icon, {...style}]}
+        />
     );
 }
+
+const styles = StyleSheet.create({
+    icon: {
+        backgroundColor: colors.primary,
+        padding: 10,
+        borderRadius: 20,
+        marginTop: 10
+    }
+})
 
 export default IconButton;
