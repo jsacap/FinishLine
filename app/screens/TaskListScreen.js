f24d2cf215c890881ab820d7d95c4f4ff59a8a70
jsacap <sanchoalegre@gmail.com>
2024-04-17 16:24:26 +1000

    TasklistScreen timer and fetching logic

--- app/screens/TaskListScreen.js
+++ app/screens/TaskListScreen.js
@@ -1,56 +1,37 @@
 import BottomSheet from "@gorhom/bottom-sheet";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import Constants from "expo-constants";
-import { useEffect, useMemo, useRef } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import Toast from "react-native-toast-message";
 import AppButton from "../components/AppButton";
+import CompletedTasks from "../components/CompletedTasks";
 import IconButton from "../components/IconButton";
 import IncompleteTaskList from "../components/IncompleteTaskList";
 import colors from "../config/colors";
 import useTaskStore from "../store/TaskStore";
 import AddTaskScreen from "./AddTaskScreen";
-import Toast from "react-native-toast-message";
-import CompletedTasks from "../components/CompletedTasks";
-import TimeCompletion from "../components/TimeCompletion";
-import TimeButton from "../components/AppText/TimeButton";
 
 export default function TaskListScreen() {
+  const [tasks, setTasks] = useState([]);
+  const [activeTaskId, setActiveTaskId] = useState(null);
+  const [timerRunning, setTimerRunning] = useState(false);
   const {
-    loadTasks,
     isBottomSheetVisible,
     openBottomSheet,
     closeBottomSheet,
     clearTaskInputs,
-    startTimer,
-    toggleTimer,
-    pauseTimer,
-    resumeTimer,
-    activeTaskId,
-    getIncompleteTasks,
   } = useTaskStore((state) => ({
-    loadTasks: state.loadTasks,
     isBottomSheetVisible: state.isBottomSheetVisible,
     openBottomSheet: state.openBottomSheet,
     closeBottomSheet: state.closeBottomSheet,
     clearTaskInputs: state.clearTaskInputs,
-    startTimer: state.startTimer,
-    timerRunning: false,
-    currentTaskId: null,
-    getIncompleteTasks: state.getIncompleteTasks,
-    toggleTimer: state.toggleTimer,
-    pauseTimer: state.pauseTimer,
-    resumeTimer: state.resumeTimer,
-    activeTaskId: state.activeTaskId,
   }));
-
+  // bottomsheet logic
   const snapPoints = useMemo(() => ["25%", "50%", "70%"]);
   const bottomSheetRef = useRef(null);
 
-  const snapToIndex = (index) => bottomSheetRef.current?.snapToIndex(index);
-
-  useEffect(() => {
-    loadTasks();
-  }, []);
-
+  // const snapToIndex = (index) => bottomSheetRef.current?.snapToIndex(index);
   useEffect(() => {
     if (isBottomSheetVisible) {
       bottomSheetRef.current?.expand();
@@ -59,37 +40,89 @@ export default function TaskListScreen() {
     }
   }, [isBottomSheetVisible]);
 
-  const getButtonTitle = () => {
-    if (!activeTaskId) return "Start";
-    const task = getIncompleteTasks().find((task) => task.id === activeTaskId);
-    return task && task.timerActive ? "Pause" : "Resume";
-  };
-  const handleTimerControl = () => {
-    const tasks = getIncompleteTasks();
-    if (!activeTaskId && tasks.length > 0) {
-      toggleTimer(tasks[0].id);
-    } else {
-      // Check if the currently active task is running and either pause or resume it
-      const activeTask = tasks.find((task) => task.id === activeTaskId);
-      if (activeTask && activeTask.timerActive) {
-        pauseTimer(activeTaskId);
-      } else {
-        resumeTimer(activeTaskId);
+  // fetching tasks
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
+  const loadTasks = async () => {
+    try {
+      const storedTasks = await AsyncStorage.getItem("tasks");
+      if (storedTasks) {
+        setTasks(JSON.parse(storedTasks));
       }
+    } catch (error) {
+      Toast.show({
+        type: "error",
+        text1: "Failed Loading Tasks",
+      });
     }
   };
+
+  const updateTaskCompletion = async (taskId) => {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, status: "complete" };
+      }
+      return task;
+    });
+
+    setTasks(updatedTasks);
+    await AsyncStorage.setItem("tasks", JSON.stringify(updatedTasks));
+  };
+
+  // timer logic
+  const handleTimer = (taskId) => {
+    setActiveTaskId(taskId);
+    setTimerRunning(true);
+    const timerInterval = setInterval(() => {
+      setTasks((currentTasks) => {
+        return currentTasks.map((task) => {
+          if (task.id === taskId) {
+            if (task.remainingSeconds > 0) {
+              return {
+                ...task,
+                remainingSeconds: TaskListScreen.remainingSeconds - 1,
+              };
+            } else {
+              clearInterval(timerInterval);
+              updateTaskCompletion(taskId);
+              setActiveTaskId(null); // change this to go down the list??
+              setTimerRunning(false); // Only false if there are no more tasks in the list?
+              return { ...task, status: "complete" };
+            }
+          }
+          return task;
+        });
+      });
+    }, 1000);
+  };
+
+  const handlePauseResume = () => {
+    setTimerRunning(!timerRunning);
+  };
+
+  // Dynamically rendering title for AppButton Start
+  const getButtonTitle = () => {
+    if (!activeTaskId) return "START";
+    return timerRunning ? "Pause" : "Resume";
+  };
+
+  // filtering for incomplete tasks
+  const incompleteTasks = tasks.filter((task) => task.status !== "complete");
+  const completedTasks = tasks.filter((task) => task.status === "complete");
 
   return (
     <View style={styles.container}>
       <View style={styles.buttons}>
         <AppButton title="Add Task" onPress={openBottomSheet} />
-        <AppButton title={getButtonTitle()} onPress={handleTimerControl} />
+        <AppButton title={getButtonTitle()} onPress={handlePauseResume} />
       </View>
       <View style={styles.incompleteList}>
-        <IncompleteTaskList />
+        <IncompleteTaskList tasks={incompleteTasks} />
       </View>
       <View style={styles.incompleteList}>
-        <CompletedTasks />
+        <CompletedTasks tasks={completedTasks} />
       </View>
       <BottomSheet
         ref={bottomSheetRef}
